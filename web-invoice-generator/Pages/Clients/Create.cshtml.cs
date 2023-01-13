82fe3f66b6d096491d367482cf3270b60e1b112a
dariusr <darius.r111@gmail.com>
2023-01-13 12:30:14 +0200

    clients list, foreach..

--- web-invoice-generator/Pages/Clients/Create.cshtml.cs
+++ web-invoice-generator/Pages/Clients/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace web_invoice_generator.Pages.Clients
 {
@@ -33,12 +34,43 @@ namespace web_invoice_generator.Pages.Clients
 
             // save the new client info into the database
 
-           
+           try
+            {
+                String connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=invoice-gen;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO clients " +
+                        "(type, name, address, code, vat_code) VALUES " +
+                        "(@type, @name, @address, @code, @vat_code);";
 
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@type", clientInfo.type);
+                        command.Parameters.AddWithValue("@name", clientInfo.name);
+                        command.Parameters.AddWithValue("@address", clientInfo.address);
+                        command.Parameters.AddWithValue("@code", clientInfo.code);
+                        command.Parameters.AddWithValue("@vat_code", clientInfo.vat_code);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                }
+
+			}
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+
+            }
 
 
             clientInfo.type = ""; clientInfo.name = ""; clientInfo.address = ""; clientInfo.code = ""; clientInfo.vat_code = "";
+            // if ok:
             successMessage = "Klientas pridëtas sëkmingai";
+            // if not, returning to the list of the clients:
+            Response.Redirect("/Clients/Index");
         }
     }
 }
