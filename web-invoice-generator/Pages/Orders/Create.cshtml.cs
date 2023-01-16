2ca0b9116bf1a5542ee3cc6f7cb20cb944385f5d
dariusr <darius.r111@gmail.com>
2023-01-16 09:57:59 +0200

    Orders crud

--- web-invoice-generator/Pages/Orders/Create.cshtml.cs
+++ web-invoice-generator/Pages/Orders/Create.cshtml.cs
@@ -21,11 +21,12 @@ namespace web_invoice_generator.Pages.Orders
 		// with method we can read date of the Form ant put to orderInfo object
 		public void OnPost()
         {
+			orderInfo.invoice_id = Request.Form["invoice_id"];
 			orderInfo.services_id = Request.Form["services_id"];
 			orderInfo.hours = Request.Form["hours"];
            
             // validating if any field empty
-            if (orderInfo.services_id.Length == 0 || orderInfo.hours.Length == 0)
+            if (orderInfo.invoice_id.Length == 0 || orderInfo.services_id.Length == 0 || orderInfo.hours.Length == 0)
             {
                 errorMessage = "Visi laukai turi bûti uþpildyti";
                 return;
@@ -39,11 +40,12 @@ namespace web_invoice_generator.Pages.Orders
                 {
                     connection.Open();
                     String sql = "INSERT INTO orders " +
-                        "(services_id, hours) VALUES " +
-						"(@services_id, @hours);";
+                        "(invoice_id, services_id, hours) VALUES " +
+						"(@invoice_id, @services_id, @hours);";
 
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
+                        command.Parameters.AddWithValue("@invoice_id", orderInfo.invoice_id);
                         command.Parameters.AddWithValue("@services_id", orderInfo.services_id);
                         command.Parameters.AddWithValue("@hours", orderInfo.hours);
 
@@ -61,7 +63,7 @@ namespace web_invoice_generator.Pages.Orders
             }
 
 
-			orderInfo.services_id = ""; orderInfo.hours = "";
+			orderInfo.invoice_id = ""; orderInfo.services_id = ""; orderInfo.hours = "";
             // if ok:
             successMessage = "Sàskaita faktûra pridëta sëkmingai";
             // if not, returning to the list of the clients:
---
+++ web-invoice-generator/Pages/Orders/Edit.cshtml.cs
@@ -0,0 +1,118 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using web_invoice_generator.Pages.Orders;
+
+namespace web_invoice_generator.Pages.Orders
+{
+    public class EditModel : PageModel
+    {
+
+        public OrderInfo orderInfo = new OrderInfo();
+        public String errorMessage = "";
+        public String successMessage = "";
+
+        public void OnGet()
+        {
+
+			// reading id
+			String id = Request.Query["id"];
+
+			// connecting to db geting info from db and filling object serviceInfo
+			try
+			{
+				// connectionString
+				String connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=invoice-gen;Integrated Security=True";
+				// creating SqlConnection
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					// open the connection
+					connection.Open();
+					// selecting service with corresponding id (@id recieved from request)
+					String sql = "SELECT * FROM orders WHERE id=@id"; 
+					// creating command which let us execute sql query
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						// replacing @id with id recieved from request
+						command.Parameters.AddWithValue("@id", id);
+						// creating SqlDataReader
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							if (reader.Read())
+							{
+								// and filling serviceInfo with data from db
+								orderInfo.id = "" + reader.GetInt32(0); //"" + int = converting int to string
+								orderInfo.invoice_id = "" + reader.GetString(1);
+								orderInfo.services_id = "" + reader.GetInt32(2);
+								orderInfo.hours = "" + reader.GetInt32(3);
+							}
+						}
+
+					}
+
+				}
+
+			}
+
+			catch (Exception ex)
+			{
+				errorMessage = ex.Message;
+			}
+		}
+
+		public void OnPost()
+		{
+			// filling orderInfo data with data from Form
+			orderInfo.id = Request.Form["id"];
+			orderInfo.invoice_id = Request.Form["invoice_id"];
+			orderInfo.services_id = Request.Form["services_id"];
+			orderInfo.hours = Request.Form["hours"];
+
+			// if form empty, displaying error message
+
+			if (orderInfo.id.Length == 0 || orderInfo.invoice_id.Length == 0 || orderInfo.services_id.Length == 0 || orderInfo.hours.Length == 0)
+			{
+				errorMessage = "Visi laukai turi bûti uþpildyti";
+				return;
+			}
+
+
+			// in the try conecting to db
+			try
+			{
+				String connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=invoice-gen;Integrated Security=True";
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					String sql = "UPDATE orders " +
+						"SET invoice_id=@invoice_id, services_id=@services_id, hours=@hours " + "WHERE id=@id";
+					// replacing @invoice_id .. with parameters from Form 
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						command.Parameters.AddWithValue("@invoice_id", orderInfo.invoice_id);
+						command.Parameters.AddWithValue("@services_id", orderInfo.services_id);
+						command.Parameters.AddWithValue("@hours", orderInfo.hours);
+						command.Parameters.AddWithValue("@id", orderInfo.id);
+
+						command.ExecuteNonQuery();
+					}
+
+				}
+
+			}
+
+			catch(Exception ex)
+			{
+				// errorMessage when we have to exit method
+				errorMessage = ex.Message;
+				return;
+			}
+			// if update Services ok, redirect to index
+			Response.Redirect("/Orders/Index");
+		}
+
+
+
+
+    }
+}
