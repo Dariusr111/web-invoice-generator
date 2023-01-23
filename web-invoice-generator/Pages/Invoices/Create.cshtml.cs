cc00549c23a53b58bf00f1aee6431b25f57fc0e0
dariusr <darius.r111@gmail.com>
2023-01-23 21:27:21 +0200

    string update

--- web-invoice-generator/Pages/Invoices/Create.cshtml.cs
+++ web-invoice-generator/Pages/Invoices/Create.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace web_invoice_generator.Pages.Invoices
 {
@@ -10,53 +12,60 @@ namespace web_invoice_generator.Pages.Invoices
 		// global variables
 		public InvoiceInfo invoiceInfo = new InvoiceInfo();
         // if any field empty we get error message 
-        public String errorMessage = "";
+        public string errorMessage = "";
         // if data saved success message
-        public String successMessage = "";
+        public string successMessage = "";
 
         public void OnGet()
         {
         }
 
-		// with method we can read date of the Form ant put to clientInfo object
+		// with method we can read date of the Form ant put to invoiceInfo object
 		public void OnPost()
         {
 			invoiceInfo.serial = Request.Form["serial"];
 			invoiceInfo.number = Request.Form["number"];
-			invoiceInfo.client_id = Request.Form["client_id"];
-			invoiceInfo.provider_id = Request.Form["provider_id"];
-			invoiceInfo.total_price = Request.Form["total_price"];
+ 
            
             // validating if any field empty
-            if (invoiceInfo.serial.Length == 0 || invoiceInfo.number.Length == 0 || invoiceInfo.client_id.Length == 0 || invoiceInfo.provider_id.Length == 0 || invoiceInfo.total_price.Length == 0)
+            if (invoiceInfo.serial.Length == 0 || invoiceInfo.number.Length == 0)
             {
                 errorMessage = "Visi laukai turi bûti uþpildyti";
                 return;
             }
 
-            // saving the new invoice info into the database
+            // saving the new order info into the database
            try
             {
-                String connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=invoice-gen;Integrated Security=True";
+				string connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=invoice-gen;Integrated Security=True";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    String sql = "INSERT INTO invoices " +
-                        "(serial, number, client_id, provider_id, total_price) VALUES " +
-                        "(@serial, @number, @client_id, @provider_id, @total_price);";
+					string sql = "INSERT INTO invoices " +
+                        "(serial, number) VALUES " +
+                    "(@serial, @number);";
 
-                    using (SqlCommand command = new SqlCommand(sql, connection))
+					// BEGIN TRANSACTION
+                    //   DECLARE @DataID int;
+					//   INSERT INTO DataTable(Column1...) VALUES(....);
+					//   SELECT @DataID = scope_identity();
+					//   INSERT INTO LinkTable VALUES(@ObjectID, @DataID);
+					// COMMIT
+
+
+
+
+
+					using (SqlCommand command = new SqlCommand(sql, connection))
                     {
                         command.Parameters.AddWithValue("@serial", invoiceInfo.serial);
                         command.Parameters.AddWithValue("@number", invoiceInfo.number);
-                        command.Parameters.AddWithValue("@client_id", invoiceInfo.client_id);
-                        command.Parameters.AddWithValue("@provider_id", invoiceInfo.provider_id);
-                        command.Parameters.AddWithValue("@total_price", invoiceInfo.total_price);
 
                         command.ExecuteNonQuery();
                     }
 
                 }
+
 
 			}
             catch (Exception ex)
@@ -67,7 +76,7 @@ namespace web_invoice_generator.Pages.Invoices
             }
 
 
-			invoiceInfo.serial = ""; invoiceInfo.number = ""; invoiceInfo.client_id = ""; invoiceInfo.provider_id = ""; invoiceInfo.total_price = "";
+			invoiceInfo.serial = ""; invoiceInfo.number = "";
             // if ok:
             successMessage = "Sàskaita faktûra pridëta sëkmingai";
             // if not, returning to the list of the clients:
@@ -75,3 +84,5 @@ namespace web_invoice_generator.Pages.Invoices
         }
     }
 }
+
+
